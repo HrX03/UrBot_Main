177b75cb2a38af9fed9ddcaf20490f7f30857d6a
Glitch (urbot-main) <none>
2017-12-29 17:20:30 +0000

    Developed with glitch

--- commands/username.js
+++ commands/username.js
@@ -2,8 +2,8 @@ exports.run = (bot, message, args) => {
     if(message.author.id === "270644886758359051") {
       if(args === '') return message.reply("Can't be empty!")
       if(args === ' ') return message.reply("Can't be empty!")
-      let avatar = args.join(' ');
-      bot.user.setAvatar(avatar)
-      message.reply(`New avatar: "${avatar}"`)
-    } else return message.reply("Only the bot owner can use this command!")
+      let username = args.join(' ');
+      bot.user.setUsername(username)
+      message.reply(`New username: "${username}"`)
+  } else return message.reply("Only the bot owner can use this command!")
 }
